d5db1e9107a66eb825d6aa4ff84e92b747564b9e
Swapnanil Dasgupta <you@example.com>
2023-04-06 01:24:05 -0500

    fixed a few bugs

--- realtime-editable-website/server.js
+++ realtime-editable-website/server.js
@@ -2,30 +2,29 @@ const http = require("http");
 const fs = require("fs");
 const socketio = require("socket.io");
 
-let document = ""; // initialize document with an empty string
-
 const server = http.createServer(function(req, res) {
   res.writeHead(200, { "Content-Type": "text/html" });
   fs.createReadStream("index.html").pipe(res);
 });
 
 const io = socketio(server);
+let userCount = 0;
+
 io.on("connection", function(socket) {
   console.log("A user connected");
 
-  // send the current document to the newly connected client
-  socket.emit("document", document);
+  userCount++;
+  io.emit("userCount", userCount);
 
-  socket.on("document", function(newDocument) {
-    // update the document variable with the new document
-    document = newDocument;
-
-    // broadcast the updated document to all connected clients
-    io.emit("document", document);
+  socket.on("text", function(text) {
+    io.emit("text", text);
   });
 
   socket.on("disconnect", function() {
     console.log("A user disconnected");
+
+    userCount--;
+    io.emit("userCount", userCount);
   });
 });
 
