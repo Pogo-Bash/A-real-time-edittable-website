9b40e7c5b09a3a58d44eac913901921cf7619c79
Swap <84603585+Pogo-Bash@users.noreply.github.com>
2023-04-06 19:39:47 -0500

    Update server.js
    edited some things so it worked with index not much tho

--- realtime-editable-website/server.js
+++ realtime-editable-website/server.js
@@ -2,32 +2,34 @@ const http = require("http");
 const fs = require("fs");
 const socketio = require("socket.io");
 
-const server = http.createServer(function(req, res) {
+const server = http.createServer(function (req, res) {
   res.writeHead(200, { "Content-Type": "text/html" });
   fs.createReadStream("index.html").pipe(res);
 });
 
 const io = socketio(server);
-let userCount = 0;
+const users = {};
 
-io.on("connection", function(socket) {
+io.on("connection", function (socket) {
   console.log("A user connected");
 
-  userCount++;
-  io.emit("userCount", userCount);
-
-  socket.on("text", function(text) {
-    io.emit("text", text);
+  socket.on("setUsername", function (username) {
+    users[socket.id] = username;
+    io.emit("users", Object.values(users));
   });
 
-  socket.on("disconnect", function() {
+  socket.on("text", function (text) {
+    io.emit("text", { text: text, users: Object.values(users) });
+  });
+
+  socket.on("disconnect", function () {
     console.log("A user disconnected");
 
-    userCount--;
-    io.emit("userCount", userCount);
+    delete users[socket.id];
+    io.emit("users", Object.values(users));
   });
 });
 
-server.listen(3000, function() {
+server.listen(3000, function () {
   console.log("Server running on port 3000");
 });
